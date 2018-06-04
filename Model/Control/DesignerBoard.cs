876d6d5feec2f5d4b12a61d3f2ada8ee25501084
Givemebuff <476752983@qq.com>
2018-06-05 01:35:01 +0800

    DataGrid

---
+++ Model/Control/DesignerBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardDesigner.Model
+{
+    [Serializable]
+    public class DesignerBoard:DesignerControl
+    {
+        
+        public DesignerBoard() 
+        {
+            this.Type = DesignerElementType.Control;
+            this.Children = new ObservableCollection<DesignerControl>();
+        } 
+        [ReadOnly(true)]
+        [Category("内容")]
+        [DisplayName("子元素集合")]
+        [Description("子元素集合")]
+        public ObservableCollection<DesignerControl> Children
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SystemControl/CopyHelper/CopyHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardDesigner
+{
+    public class CopyHelper
+    {
+        public static object DeepCopy(object obj)
+        {
+            //如果是字符串或值类型则直接返回
+            if (obj is string || obj.GetType().IsValueType) return obj;
+
+            object retval = Activator.CreateInstance(obj.GetType());
+            FieldInfo[] fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (FieldInfo field in fields)
+            {
+                try { field.SetValue(retval, DeepCopy(field.GetValue(obj))); }
+                catch { }
+            }
+            return retval;
+        }
+    }
+}
